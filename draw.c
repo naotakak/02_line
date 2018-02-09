54b4d02b19dc2c4da9fe31b0a50d46d82602f70a
naotakak <naotakakinoshita@gmail.com>
2018-02-08 19:18:46 -0500

    octant I

---
+++ draw.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+
+//Insert your line algorithm here
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+
+  //octant I
+  int x = x0;
+  int y = y0;
+  int A = y1 - y0;
+  int B = -1 * (x1 - x0);
+  int d = 2 * A + B;
+
+  while (x <= x1) {
+    plot(s, c, x, y);
+    if (d > 0) {
+      y ++;
+      d += (2 * B);
+    }
+    x ++;
+    d += (2 * A);
+  }
+}
