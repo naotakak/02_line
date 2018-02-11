df0522966906a06e40f62ea0bf12dc9dc6cc6ba2
naotakak <naotakakinoshita@gmail.com>
2018-02-10 23:14:59 -0500

    done

--- draw.c
+++ draw.c
@@ -7,21 +7,66 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int x, y, A, B, d;
+
+  if (x0 > x1) { //switch order of points
+    draw_line(x1, y1, x0, y0, s, c);
+  }
+  x = x0;
+  y = y0;
+  B = -1 * (x1 - x);
+  A = y1 - y;
+  d = 2 * A + B;
 
   //octant I
-  int x = x0;
-  int y = y0;
-  int A = y1 - y0;
-  int B = -1 * (x1 - x0);
-  int d = 2 * A + B;
+  if (A >= 0 && (-1 * B) >= A) {
+    while (x <= x1) {
+      plot(s, c, x, y);
+      if (d > 0) {
+        y ++;
+        d += (2 * B);
+      }
+      x ++;
+      d += (2 * A);
+    }
+  }
 
-  while (x <= x1) {
-    plot(s, c, x, y);
-    if (d > 0) {
+  //octant II
+  else if (A >= 0 && (-1 * B) <= A) {
+    while (y <= y1) {
+      plot(s, c, x, y);
+      if (d < 0) {
+        x ++;
+        d += (2 * A);
+      }
       y ++;
       d += (2 * B);
     }
-    x ++;
-    d += (2 * A);
+  }
+
+  //octant VIII
+  else if (A < 0 && B <= A) {
+    while (x <= x1) {
+      plot(s, c, x, y);
+      if (d > 0) {
+        y --;
+        d += (2 * B);
+      }
+      x ++;
+      d -= (2 * A);
+    }
+  }
+
+  //octant VII
+  else if (A < 0 && B >= A) {
+    while (y >= y1) {
+      plot(s, c, x, y);
+      if (d < 0) {
+        x ++;
+        d -= (2 * A);
+      }
+      y --;
+      d += (2 * B);
+    }
   }
 }
--- main.c
+++ main.c
@@ -11,13 +11,63 @@ int main() {
   screen s;
   color c;
 
-  c.red = 0;
+  c.red = MAX_COLOR;
   c.green = MAX_COLOR;
-  c.blue = 0;
+  c.blue = MAX_COLOR;
 
   clear_screen(s);
 
-  draw_line(10, 10, 100, 100, s, c);
+  //triangle
+  draw_line(200, 200, 300, 200, s, c);
+  draw_line(250, 300, 300, 200, s, c);
+  draw_line(200, 200, 250, 300, s, c);
+
+  //ray
+  draw_line(0, 200, 225, 250, s, c);
+
+  //refract I
+  for (int i = -10; i < 10; i += 4) {
+    draw_line(225, 250, 250, 250 + i, s, c);
+  }
+
+  //refract II
+  c.red = 255;
+  c.green = 0;
+  c.blue = 0;
+  for (int i = 0; i < 5; i ++) {
+    draw_line(270 + i, 260 - i, 500, 225 - i, s, c);
+  }
+
+  c.green = 153;
+  for (int i = 0; i < 5; i ++) {
+    draw_line(273 + i, 255 - i, 500, 220 - i, s, c);
+  }
+
+  c.green = 255;
+  for (int i = 0; i < 5; i ++) {
+    draw_line(276 + i, 250 - i, 500, 215 - i, s, c);
+  }
+
+  c.red = 0;
+  c.green = 102;
+  for (int i = 0; i < 5; i ++) {
+    draw_line(279 + i, 245 - i, 500, 210 - i, s, c);
+  }
+
+  c.red = 51;
+  c.green = 153;
+  c.blue = 255;
+  for (int i = 0; i < 5; i ++) {
+    draw_line(282 + i, 240 - i, 500, 205 - i, s, c);
+  }
+
+  c.red = 153;
+  c.green = 51;
+  c.blue = 255;
+  for (int i = 0; i < 5; i ++) {
+    draw_line(285 + i, 235 - i, 500, 200 - i, s, c);
+  }
+
   display(s);
   save_extension(s, "lines.png");
 }
